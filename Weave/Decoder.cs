21d288471142cb0a5af2b1b07159d578860876b1
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-11-22 15:22:17 +0100

    Initial commit

---
+++ Weave/Decoder.cs
@@ -0,0 +1,30 @@
+﻿namespace Weave;
+
+public class Decoder
+{
+    private readonly byte[] _message;
+
+    public Decoder(byte[] message)
+    {
+        _message = message;
+    }
+
+    public byte[] Decode(byte xorParam)
+    {
+        var message = new byte[_message.Length];
+        var ecx = _message.Length;
+        for (var esi = 0; esi < ecx; esi++)
+        {
+            var al = _message[esi] ^ xorParam;
+            unsafe
+            {
+                // C# forces byte xor byte to be an int result and wont
+                // allow a int -> byte cast, so we use an unsafe pointer
+                var ptr = (byte*)&al;
+                message[esi] = *ptr;
+            }
+        }
+
+        return message;
+    }
+}
